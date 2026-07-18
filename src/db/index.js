60cedab1d0fcabf4c4dd3da7be2dedb065b22208
Birendra Chandravanshi <virendrargtu@gmail.com>
2026-07-18 16:40:44 +0530

    util code disccussion

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,11 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
 
-     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-     console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+     const connectionInstance = await mongoose.
+     connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+     
+     console.log(`MongoDB connected !! DB HOST: 
+      ${connectionInstance.connection.host}`);
 
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 import dns from 'node:dns'; // or const dns = require('node:dns');
+import { error } from 'node:console';
 dns.setServers(['1.1.1.1', '8.8.8.8']); 
-
 
 dotenv.config(
     {
@@ -10,9 +10,19 @@ dotenv.config(
     }
 );
 
-connectDB();
-
-
+connectDB()
+.then(()=>{
+     app.on(error, (error)=>{
+        console.log('Error connecting to MongoDB:', error);
+    })
+    app.listen(process.env.PORT || 8000, ()=>
+    {
+        console.log(`server is running at port: ${process.env.PORT}`);
+    });   
+})
+.catch((error) =>{
+    console.error('Error connecting to MongoDB:', error);
+})
 
 
 /*
